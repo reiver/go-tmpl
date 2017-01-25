309a6f4c54fcca54a4bfe264b0a5b7ce299413d4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-01-25 11:29:37 -0800

    examples

---
+++ print_examples_test.go
@@ -0,0 +1,70 @@
+package tmpl_test
+
+import (
+	"github.com/reiver/go-tmpl"
+
+	"io"
+	"os"
+)
+
+func ExampleFprintt() {
+
+	type Person struct {
+		Name string
+		Age  int
+		Sex  string
+	}
+
+	var person Person
+
+	person.Name = "Joe Blow"
+	person.Age  = 41
+	person.Sex  = "male"
+
+	tmpl.Fprintt(os.Stdout, "Hello {{.Name}}, you are {{.Age}} years old.", person)
+
+	// Output:
+	// Hello Joe Blow, you are 41 years old.
+}
+
+func ExamplePrintt() {
+
+	type Person struct {
+		Name string
+		Age  int
+		Sex  string
+	}
+
+	var person Person
+
+	person.Name = "Joe Blow"
+	person.Age  = 41
+	person.Sex  = "male"
+
+	tmpl.Printt("Hello {{.Name}}, you are {{.Age}} years old.", person)
+
+	// Output:
+	// Hello Joe Blow, you are 41 years old.
+}
+
+func ExampleSprintt() {
+
+	type Person struct {
+		Name string
+		Age  int
+		Sex  string
+	}
+
+	var person Person
+
+	person.Name = "Joe Blow"
+	person.Age  = 41
+	person.Sex  = "male"
+
+	s := tmpl.Sprintt("Hello {{.Name}}, you are {{.Age}} years old.", person)
+
+	io.WriteString(os.Stdout, s)
+
+	// Output:
+	// Hello Joe Blow, you are 41 years old.
+}
