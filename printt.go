e247daf3e4d388d021606834f57b62451031dbfe
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-01-25 11:48:21 -0800

    tmpl.Printt()

--- printt.go
+++ printt.go
@@ -4,6 +4,8 @@ import (
 	"os"
 )
 
+// Printt renders according to a template bound to the data and writes to standard output.
+// It returns the number of bytes written and any write error encountered.
 func Printt(template string, data interface{}) (n int, err error) {
 	return Fprintt(os.Stdout, template, data)
 }
